9f632b339b853d49a8b88146cb608b5e5c31c602
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2023-07-05 14:43:22 -0300

    Rutas vista y crud creadas

--- routes/reserva.routes.js
+++ routes/reserva.routes.js
@@ -2,32 +2,48 @@
 
 const router = require('express').Router();
 
+const {
+    renderListaReservas,
+    renderFormNuevaReserva,
+    renderFormEditarReserva,
+    obtenerReservas, 
+    obtenerReserva, 
+    crearReserva,
+    actualizarReserva,
+    eliminarReserva
+} = require('../controllers/reserva.controllers');
 
 // ==========================================
 //         Rutas para renderizar vistas
 // ==========================================
 
-// Obtener todas las reservas
+// Obtener todas las reservas 
+router.get('/', renderListaReservas)
 
 // Formulario para crear una reserva
+router.get('/crear-reserva', renderFormNuevaReserva)
 
 // Formulario para actualizar una reserva
+router.get('/actualizar-reserva/:id', renderFormEditarReserva) 
 
 // ==========================================
 //         Rutas para CRUD de reservas
 // ==========================================
 
 // Obtener todas las reservas
-router.get('/api/',);
- 
+router.get('/api',obtenerReservas);
+
 // Crear una reserva
-router.post('/api/',);
- 
+router.post('/api', crearReserva);
+
+router.get('/api/:id', obtenerReserva);
+
 // Actualizar una reserva
-router.put('/api/:id',);
- 
+router.put('/api/:id', actualizarReserva);
+
 // Eliminar una reserva de forma lógica
-router.delete('/api/:id',);
+router.delete('/api/:id', eliminarReserva);
+
 
  
  module.exports = router;
